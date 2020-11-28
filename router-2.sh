a57a4800aa06f8be52aea17c40d31c7be76fb8be
calorechiara <74462731+calorechiara@users.noreply.github.com>
2020-11-28 10:58:01 +0100

    Update router-2.sh

--- router-2.sh
+++ router-2.sh
@@ -10,4 +10,4 @@ sudo ip addr add 10.0.4.1/23 dev enp0s8
 sudo ip link set dev enp0s8 up
 #Replace the default gateway
 sudo ip route add 10.0.4.0/23 via 10.1.1.2
-sudo ip route add 10.0.0.0/23 via 10.1.1.1
+sudo ip route add 10.0.0.0/22 via 10.1.1.1
