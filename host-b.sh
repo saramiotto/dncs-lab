bdc6d1993036d4bdd95ad96a1167edc28ebf4ee4
calorechiara <74462731+calorechiara@users.noreply.github.com>
2020-11-28 11:24:13 +0100

    Update host-b.sh

--- host-b.sh
+++ host-b.sh
@@ -6,4 +6,4 @@ sudo ip addr add 10.0.2.2/23 dev enp0s8
 sudo ip link set dev enp0s8 up
 sudo ip route add 10.1.1.0/30 via 10.0.2.1
 #Defaul gateway set up
-sudo ip route add 10.0.0.0/21 via 10.0.2.1
+sudo ip route add 10.0.0.0/23 via 10.0.2.1
