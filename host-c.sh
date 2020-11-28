1a42432a622f54b4f9561d5fc9197f34565923da
calorechiara <74462731+calorechiara@users.noreply.github.com>
2020-11-28 11:24:26 +0100

    Update host-c.sh

--- host-c.sh
+++ host-c.sh
@@ -14,4 +14,4 @@ sudo ip addr add 10.0.4.2/23 dev enp0s8
 sudo ip link set dev enp0s8 up
 #Defaul gateway set up
 sudo ip route add 10.1.1.0/30 via 10.0.4.1
-sudo ip route add 10.0.0.0/21 via 10.0.4.1
+sudo ip route add 10.0.0.0/23 via 10.0.4.1
