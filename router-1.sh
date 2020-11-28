97777fdd5f251a2857cd9a3001217139d2d2b4d7
calorechiara <74462731+calorechiara@users.noreply.github.com>
2020-11-28 11:22:47 +0100

    Update router-1.sh

--- router-1.sh
+++ router-1.sh
@@ -9,8 +9,8 @@ sudo ip link set dev enp0s9 up
 sudo ip link set dev enp0s8 up
 #VLAN interfaces
 sudo ip link add link enp0s8 name enp0s8.2 type vlan id 2
-sudo ip addr add 10.0.0.1/23 dev enp0s8.2
 sudo ip link add link enp0s8 name enp0s8.3 type vlan id 3
+sudo ip addr add 10.0.0.1/23 dev enp0s8.2
 sudo ip addr add 10.0.2.1/23 dev enp0s8.3
 #Access to Host-C subnet
 sudo ip route add 10.0.4.0/23 via 10.1.1.2
